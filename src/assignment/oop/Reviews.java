a9fe03c186f322d346b4a5a9f827af280afa9f71
Fishylord <chunyee04@outlook.com>
2023-12-02 10:04:09 +0800

    Merge origin/master
    Conflicts:
    	path_to_orders_file.txt

--- src/assignment/oop/Reviews.java
+++ src/assignment/oop/Reviews.java
@@ -12,50 +12,40 @@ public class Reviews {
    private String foodId;
     private int rating;
     private String reviewMsg;
+    private int runnerrating;
+    private String runnerreviewMsg;
     private String userId;
+    private String runnerId;
 
-    public Reviews(String foodId, int rating, String reviewMsg, String userId) {
+    public Reviews(String foodId, int rating, String reviewMsg, int runnerrating, String runnerreviewMsg, String userId, String runnerId) {
         this.foodId = foodId;
         this.rating = rating;
         this.reviewMsg = reviewMsg;
+        this.runnerrating = runnerrating;
+        this.runnerreviewMsg = runnerreviewMsg;
         this.userId = userId;
+        this.runnerId = runnerId;
     }
 
-    // Getters and Setters
-    public String getFoodId() {
-        return foodId;
-    }
-
-    public void setFoodId(String foodId) {
-        this.foodId = foodId;
-    }
-
-    public int getRating() {
-        return rating;
-    }
+   
+    public String getFoodId() {return foodId;}
+    public void setFoodId(String foodId) {this.foodId = foodId;}
+    public int getRating() {return rating;}
+    public void setRating(int rating) {this.rating = rating;}
+    public String getReviewMsg() {return reviewMsg;}
+    public void setReviewMsg(String reviewMsg) {this.reviewMsg = reviewMsg;}
+    public String getUserID() {return userId;}      
+    public void setUserId(String userId){this.userId = userId;}
     
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public String getReviewMsg() {
-        return reviewMsg;
-    }
-
-    public void setReviewMsg(String reviewMsg) {
-        this.reviewMsg = reviewMsg;
-    }
-    
-    public String getUserID() {
-        return userId;
-    }
-            
-    public void setUserId(String userId){
-        this.userId = userId;
-    }
+    public int getRunnerRating() {return runnerrating;}
+    public void setRunnerRating(int runnerrating) {this.runnerrating = runnerrating;}
+    public String getRunnerReviewMsg() {return runnerreviewMsg;}
+    public void setRunnerReviewMsg(String runnerreviewMsg) {this.runnerreviewMsg = runnerreviewMsg;}
+    public String getRunnerId() {return runnerId;}
+    public void setRunnerId(String runnerId) {this.runnerId = runnerId;}
+    //Remember delivery driver will not be seeing the rating and reviewMsg for the food. just foodid rating reviewMsg.
     @Override
     public String toString() {
-        return "Food ID: " + foodId + ", Rating: " + rating + ", Review: " + reviewMsg + ", User ID: " + userId;
+        return "Food ID: " + foodId + ", Rating: " + rating + ", Review: " + reviewMsg + "Runner's Rating: " + runnerrating + "Runner's Review" + runnerreviewMsg + ", User ID: " + userId + ", Runner's ID:" + runnerId;
     }
 }
